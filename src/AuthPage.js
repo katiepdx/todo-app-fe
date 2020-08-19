0b237b1b2928065ed09fa6753566f093b8667516
KatiePDX <61799774+katiepdx@users.noreply.github.com>
2020-08-19 14:48:47 -0700

    add signin functionality

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import { signUpUser } from './todos-api.js';
+import { signUpUser, signInUser } from './todos-api.js';
 
 export default class AuthPage extends Component {
     // set state 
     state = {
         signUpEmail: '',
-        signUpPassword: ''
+        signUpPassword: '',
+        signInEmail: '',
+        signInPassword: ''
     };
 
-    handleEmailChange = (e) => {
+    // Signup handle methods
+    handleSignUpEmailChange = (e) => {
         this.setState({ signUpEmail: e.target.value})
     }
 
-    handlePasswordChange = (e) => {
+    handleSignUpPasswordChange = (e) => {
         this.setState({ signUpPassword: e.target.value})
+    }
+
+    // Signin handle methods
+    handleSignInEmailChange = (e) => {
+        this.setState({ signInEmail: e.target.value})
+    }
+
+    handleSignInPasswordChange = (e) => {
+        this.setState({ signInPassword: e.target.value})
     }
 
     handleSignUpClick = async (e) => {
@@ -26,7 +38,24 @@ export default class AuthPage extends Component {
             password: this.state.signUpPassword
         })
 
-        // pass token
+        // pass received user token and set to state and localStorage
+        this.props.handleToken(user.body.token)
+
+        // redirect to My Todos page
+        this.props.history.push('/todos')
+    }
+
+    handleSignInClick = async (e) => {
+        e.preventDefault();
+
+        // pass user data to signInUser function
+        const user = await signInUser({
+            // keys must match backend keys 
+            email: this.state.signInEmail,
+            password: this.state.signInPassword
+        })
+
+        // pass received user token and set to state and localStorage
         this.props.handleToken(user.body.token)
 
         // redirect to My Todos page
@@ -38,17 +67,30 @@ export default class AuthPage extends Component {
         return (
             <div>
                 <form className="signup-form" onSubmit={this.handleSignUpClick}>
-                    Sign up
+                    Sign Up
                     <label>
                         Email: 
-                        <input onChange={this.handleEmailChange} value={this.state.signUpEmail}/>
+                        <input onChange={this.handleSignUpEmailChange} value={this.state.signUpEmail}/>
                     </label>
                     <label>
                         Password: 
-                        <input onChange={this.handlePasswordChange} value={this.state.signUpPassword}/>
+                        <input onChange={this.handleSignUpPasswordChange} value={this.state.signUpPassword}/>
                     </label>
 
                     <button>Sign Up!</button>
+                </form>
+                <form className="signin-form" onSubmit={this.handleSignInClick}>
+                    Sign In
+                    <label>
+                        Email: 
+                        <input onChange={this.handleSignInEmailChange} value={this.state.signInEmail}/>
+                    </label>
+                    <label>
+                        Password: 
+                        <input onChange={this.handleSignInPasswordChange} value={this.state.signInPassword}/>
+                    </label>
+
+                    <button>Sign In!</button>
                 </form>
             </div>
         )
--- src/todos-api.js
+++ src/todos-api.js
@@ -3,11 +3,20 @@ import request from 'superagent';
 const URL = process.env.REACT_APP_API_URL;
 
 // signup function 
-
 export function signUpUser(userData) {
     try {
         // make POST req to signup endpoint and send the userData
         return request.post(`${URL}/auth/signup`, userData)
+    } catch(e) {
+        throw { error: e.message }
+    }
+}
+
+// signin function 
+export function signInUser(userData) {
+    try {
+        // make POST req to signin endpoint and send the userData
+        return request.post(`${URL}/auth/signin`, userData)
     } catch(e) {
         throw { error: e.message }
     }
